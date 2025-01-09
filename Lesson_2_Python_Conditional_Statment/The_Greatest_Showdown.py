da97abe5d804fe4aa1ee605a154f1ca3f3b42c69
BlackBeltCoder80 <blackbeltcoder80@gmail.com>
2025-01-09 01:50:42 -0700

    Updated

---
+++ Lesson_2_Python_Conditional_Statment/The_Greatest_Showdown.py
@@ -0,0 +1,11 @@
+# Task 1: Identify the Greatest 
+# Write a Python program that asks the user to enter three numbers. 
+# Your code should then identify and print out the largest number among the three.
+
+number_1 = int(input ("Enter the first numebr that comes to your mind: "))
+numeber_2 = int(input("Enter the second numer that comes to your mint:"))
+numeber_3 = int(input("Enter the final number that comes to your mind: "))
+if number_1 > numeber_2 and number_1 > numeber_3:
+    print(number_1 is "is the Greatest number")
+    
+                   
