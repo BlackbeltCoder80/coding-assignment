623d301ff8d1799f34f17bc8c3054de9094d8467
BlackBeltCoder80 <blackbeltcoder80@gmail.com>
2025-01-22 17:30:34 -0700

    Update

---
+++ Lesson_5_Pyhon_Functions/Input_Practice_3.py
@@ -0,0 +1,14 @@
+number_string ="123"
+number = int(number_string)
+
+decimal_string ="3.14"
+pi = float(decimal_string)
+
+age =  25
+age_string = str(age)
+is_empty = float(decimal_string)
+
+print(pi + 2)
+print("I am " + age_string + " years old.")
+print(is_empty)
+
